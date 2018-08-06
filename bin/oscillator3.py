ee3e86575fab7442b2ab467de9742d21ee5a57ea
Gary Boodhoo <gboodhoo@gmail.com>
2018-08-05 17:14:36 -0700

    adapted to take outgoing minmax values as an argument

--- bin/oscillator3.py
+++ bin/oscillator3.py
@@ -2,7 +2,8 @@ from matplotlib import pyplot as plt
 from scipy import signal as sg
 import math, time
 
-def oscillator(cycle_length, frequency=1, type='sin'):
+
+def oscillator(cycle_length, frequency=1, out_minmax=[1,10], type='sin'):
     timecounter = 0
     while True:
         timecounter += 1
@@ -11,22 +12,21 @@ def oscillator(cycle_length, frequency=1, type='sin'):
             value = sg.square(2 * math.pi * frequency * timecounter / cycle_length)
         elif type=='saw':
             value = sg.sawtooth(2 * math.pi * frequency * timecounter / cycle_length)
-        yield value
+        yield remap(value, out_minmax)
 
-def remap(input_value, input_minmax, output_minmax):
+def remap(in_value, in_minmax=[-1,1], out_minmax=[-1,1]):
     return (
-            input_value - input_minmax[0]
+            in_value - in_minmax[0]
         ) / (
-            input_minmax[1] - input_minmax[0]
+            in_minmax[1] - in_minmax[0]
         ) * (
-            output_minmax[1] - output_minmax[0]
-        ) + output_minmax[0]
+            out_minmax[1] - out_minmax[0]
+        ) + out_minmax[0]
 
-cycle = oscillator(50,frequency=1.2)
+cycle = oscillator(50, out_minmax=[0,5])
 plot_list = []
 for i in range(100):
-    value = remap(cycle.next(), [-1,1], [1.1, 1.8])
-    plot_list.append(value)
+    plot_list.append(cycle.next())
 
 plt.plot(plot_list)
 plt.show()
