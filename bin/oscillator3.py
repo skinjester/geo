3a47706a59e105d84e18b6d7cc58f218ba54c68c
Gary Boodhoo <gboodhoo@gmail.com>
2018-08-07 00:58:58 -0700

    testing

--- bin/oscillator3.py
+++ bin/oscillator3.py
@@ -3,15 +3,16 @@ from scipy import signal as sg
 import math, time
 
 
-def oscillator(cycle_length, frequency=1, range_in=[-1,1], range_out=[-1,1], type='sin'):
+def oscillator(cycle_length, frequency=1, range_in=[-1,1], range_out=[-1,1], wavetype='sin'):
     timecounter = 0
     while True:
         timecounter += 1
-        value = math.sin(2 * math.pi * frequency * timecounter / cycle_length)
-        if type=='square':
+        if wavetype=='square':
             value = range_out[0] + ((range_out[1] - range_out[0]) / 2) + sg.square(2 * math.pi * frequency * timecounter / cycle_length) * ((range_out[1] - range_out[0]) / 2)
-        elif type=='saw':
+        if wavetype=='saw':
             value = sg.sawtooth(2 * math.pi * frequency * timecounter / cycle_length)
+        if wavetype=='sin':
+            value = math.sin(2 * math.pi * frequency * timecounter / cycle_length)
         yield round(value,2)
 
 def remap(value, range_in, range_out):
@@ -19,10 +20,10 @@ def remap(value, range_in, range_out):
 
 
 
-cycle = oscillator(50, frequency=1, range_out=[3,3], type="square")
+cycle = oscillator(50, frequency=1, wavetype="sin")
 plot_list = []
 for i in range(100):
-    plot_list.append(int(cycle.next()))
+    plot_list.append(cycle.next())
 
 print plot_list
 plt.plot(plot_list)
