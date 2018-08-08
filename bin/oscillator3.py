52a315a1aa6ba8e481b4afbf2cdd355b77147ed1
Gary Boodhoo <gboodhoo@gmail.com>
2018-08-07 22:16:15 -0700

    tested min, max values for sin and square waves

--- bin/oscillator3.py
+++ bin/oscillator3.py
@@ -3,24 +3,23 @@ from scipy import signal as sg
 import math, time
 
 
-def oscillator(cycle_length, frequency=1, range_in=[-1,1], range_out=[-1,1], wavetype='sin'):
+def oscillator(cycle_length, frequency=1, range_in=[-1,1], range_out=[-1,1], wavetype='sin', dutycycle=0.5):
     timecounter = 0
     while True:
         timecounter += 1
         if wavetype=='square':
-            value = range_out[0] + ((range_out[1] - range_out[0]) / 2) + sg.square(2 * math.pi * frequency * timecounter / cycle_length) * ((range_out[1] - range_out[0]) / 2)
-        if wavetype=='saw':
-            value = sg.sawtooth(2 * math.pi * frequency * timecounter / cycle_length)
-        if wavetype=='sin':
-            value = math.sin(2 * math.pi * frequency * timecounter / cycle_length)
-        yield round(value,2)
+            value = range_out[0] + ((range_out[1] - range_out[0]) / 2) + sg.square(2 * math.pi * frequency * timecounter / cycle_length, duty=dutycycle) * ((range_out[1] - range_out[0]) / 2)
+        elif wavetype=='saw':
+            value = range_out[0] + ((range_out[1] - range_out[0]) / 2) + sg.sawtooth(2 * math.pi * frequency * timecounter / cycle_length) * ((range_out[1] - range_out[0]) / 2)
+        else:
+            value = range_out[0] + ((range_out[1] - range_out[0]) / 2) + math.sin(2 * math.pi * frequency * timecounter / cycle_length) * ((range_out[1] - range_out[0]) / 2)
+        yield value
 
 def remap(value, range_in, range_out):
     return range_out[0] + (range_out[1] - range_out[0]) * ((value - range_in[0]) / (range_in[1] - range_in[0]))
 
 
-
-cycle = oscillator(50, frequency=1, wavetype="sin")
+cycle = oscillator(50, frequency=1, range_out=[1.0,1.2], wavetype="sin", dutycycle=0.9)
 plot_list = []
 for i in range(100):
     plot_list.append(cycle.next())
