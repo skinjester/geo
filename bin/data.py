11f57f332a30d664f2143d32cd3feb731a4dccf9
Gary Boodhoo <gboodhoo@gmail.com>
2018-08-12 16:13:01 -0700

    support files for test program

---
+++ bin/data.py
@@ -0,0 +1,10 @@
+import cv2
+import logging
+import logging.config
+import logsettings
+logging.config.dictConfig(logsettings.LOGGING_CONFIG)
+
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
