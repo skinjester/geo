26c0b790fc4b79d638839b2a557ea152ef20d666
Gary Boodhoo <gboodhoo@gmail.com>
2018-11-15 23:03:48 -0800

    added dedicated module to store layer names

---
+++ src/performance/layers.py
@@ -0,0 +1,390 @@
+googlenet = [
+    {
+        'name': 'conv2/3x3_reduce',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'inception_3a/3x3_reduce',
+        'features': range(-1,95)
+    },
+    {
+        'name': 'inception_3a/3x3',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_3a/5x5',
+        'features': range(-1,31)
+    },
+    {
+        'name': 'inception_3a/pool',
+        'features': range(-1,191)
+    },
+    {
+        'name': 'inception_3a/pool_proj',
+        'features': range(-1,31)
+    },
+    {
+        'name': 'inception_3a/output',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'inception_3b/1x1',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_3b/3x3_reduce',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_3b/3x3',
+        'features': range(-1,191)
+    },
+    {
+        'name': 'inception_3b/5x5_reduce',
+        'features': range(-1,32)
+    },
+    {
+        'name': 'inception_3b/5x5',
+        'features': range(-1,95)
+    },
+    {
+        'name': 'inception_3b/pool',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'inception_3b/pool_proj',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'inception_3b/output',
+        'features': range(-1,479)
+    },
+    {
+        'name': 'inception_4a/1x1',
+        'features': range(-1,191)
+    },
+    {
+        'name': 'inception_4a/3x3_reduce',
+        'features': range(-1,95)
+    },
+    {
+        'name': 'inception_4a/3x3',
+        'features': range(-1,207)
+    },
+    {
+        'name': 'inception_4a/5x5_reduce',
+        'features': range(-1,15)
+    },
+    {
+        'name': 'inception_4a/5x5',
+        'features': range(-1,47)
+    },
+    {
+        'name': 'inception_4a/pool',
+        'features': range(-1,479)
+    },
+    {
+        'name': 'inception_4a/pool_proj',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'inception_4a/output',
+        'features': range(-1,511)
+    },
+
+    {
+        'name': 'inception_4b/1x1',
+        'features': range(-1,159)
+    },
+    {
+        'name': 'inception_4b/3x3_reduce',
+        'features': range(-1,112)
+    },
+    {
+        'name': 'inception_4b/3x3',
+        'features': range(-1,224)
+    },
+    {
+        'name': 'inception_4b/5x5_reduce',
+        'features': range(-1,23)
+    },
+    {
+        'name': 'inception_4b/output',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4b/pool',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4b/pool_proj',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'inception_4c/1x1',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_4c/3x3',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'inception_4c/3x3_reduce',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_4c/5x5',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'inception_4c/5x5_reduce',
+        'features': range(-1,23)
+    },
+    {
+        'name': 'inception_4c/output',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4c/pool',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4d/3x3',
+        'features': range(-1,288)
+    },
+    {
+        'name': 'inception_4d/5x5',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'inception_4d/5x5_reduce',
+        'features': range(-1,31)
+    },
+    {
+        'name': 'inception_4d/output',
+        'features': range(-1,527)
+    },
+    {
+        'name': 'inception_4d/pool',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4e/1x1',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'inception_4e/3x3',
+        'features': range(-1,319)
+    },
+    {
+        'name': 'inception_4c/5x5_reduce',
+        'features': range(-1,23)
+    },
+    {
+        'name': 'inception_4c/output',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4c/pool',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4d/3x3',
+        'features': range(-1,288)
+    },
+    {
+        'name': 'inception_4d/5x5',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'inception_4d/output',
+        'features': range(-1,528)
+    },
+    {
+        'name': 'inception_4d/pool',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'inception_4e/1x1',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'inception_4e/3x3',
+        'features': range(-1,319)
+    },
+    {
+        'name': 'inception_4e/3x3_reduce',
+        'features': range(-1,159)
+    },
+    {
+        'name': 'inception_4e/5x5',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_4e/5x5_reduce',
+        'features': range(-1,32)
+    },
+    {
+        'name': 'inception_4e/output',
+        'features': range(-1,831)
+    },
+    {
+        'name': 'inception_4e/pool',
+        'features': range(-1,527)
+    },
+    {
+        'name': 'inception_4e/pool_proj',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_5a/1x1',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'inception_5a/3x3',
+        'features': range(-1,319)
+    },
+    {
+        'name': 'inception_5a/3x3_reduce',
+        'features': range(-1,159)
+    },
+    {
+        'name': 'inception_5a/5x5',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_5a/5x5_reduce',
+        'features': range(-1,31)
+    },
+    {
+        'name': 'inception_5a/output',
+        'features': range(-1,831)
+    },
+    {
+        'name': 'inception_5a/pool',
+        'features': range(-1,831)
+    },
+    {
+        'name': 'inception_5b/1x1',
+        'features': range(-1,383)
+    },
+    {
+        'name': 'inception_5b/3x3',
+        'features': range(-1,383)
+    },
+    {
+        'name': 'inception_5b/3x3_reduce',
+        'features': range(-1,191)
+    },
+    {
+        'name': 'inception_5b/5x5',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'inception_5b/5x5_reduce',
+        'features': range(-1,47)
+    },
+    {
+        'name': 'inception_5b/output',
+        'features': range(-1,1023)
+    },
+    {
+        'name': 'inception_5b/pool',
+        'features': range(-1,831)
+    },
+    {
+        'name': 'inception_5b/pool_proj',
+        'features': range(-1,127)
+    },
+]
+
+vgg19 = [
+    {
+        'name': 'conv1_1',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'conv1_2',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'pool1',
+        'features': range(-1,63)
+    },
+    {
+        'name': 'conv2_1',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'conv2_2',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'pool2',
+        'features': range(-1,127)
+    },
+    {
+        'name': 'conv3_1',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'conv3_2',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'conv3_3',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'conv3_4',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'pool3',
+        'features': range(-1,255)
+    },
+    {
+        'name': 'conv4_1',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv4_2',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv4_3',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv4_4',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'pool4',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv5_1',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv5_2',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv5_2',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv5_3',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'conv5_4',
+        'features': range(-1,511)
+    },
+    {
+        'name': 'pool5',
+        'features': range(-1,511)
+    },
+]
